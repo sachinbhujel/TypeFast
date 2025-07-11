ee4a66de18e33a2f721b9a6a2b5e50c9e0044359
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-07-11 05:17:45 +0000

    commit

--- TypeFast/src/About.jsx
+++ TypeFast/src/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <>
             <Link to="/" className="home-button">Back To Home</Link>
             <div className="about-container">
-                <h1>About TypeFast</h1>
+                <div>About TypeFast</div>
                 <p>
                     <strong>TypeFast</strong> is a modern typing speed test
                     platform designed to help users improve their typing
